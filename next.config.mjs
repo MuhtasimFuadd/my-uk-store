8d5f79ca1e56175e0b2f8ffe7ca4b9f72cdf28f9
MuhtasimFuadd <muhtasimfuad1000@gmail.com>
2026-09-26 11:46:16 +0600

    Bug Fix

--- next.config.mjs
+++ next.config.mjs
@@ -4,6 +4,9 @@ const nextConfig = {
     remotePatterns: [
       { protocol: "https", hostname: "**" }
     ]
+  },
+  experimental: {
+    serverComponentsExternalPackages: ["@supabase/supabase-js"]
   }
 };
 
